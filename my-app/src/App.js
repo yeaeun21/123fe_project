ff346208ce83af1a569acb6dbb15473dec024900
yeaeun21 <yeaeun28@naver.com>
2022-05-28 21:25:06 +0900

    0528

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,60 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'
+import moment, { Moment as MomentTypes } from 'moment';
 
 
 function App() {
+  const [value, onChange] = useState(new Date());
 
-    const date= new Date();
+  return (
+    <div>
+      <Calendar onChange={onChange} value={value}  />
+      <hr></hr>
+      <div className="text-gray-500 mt-4">
+        
+           {moment(value).format("YYYY.MM.DD")} 
+           
+         </div>
+         
+    </div>
+    
+  );
 
-    const viewYear = date.getFullYear();
-    const viewMonth = date.getMonth();
-
-    const prevLast = new Date(viewYear, viewMonth, 0);
-    const thisLast = new Date(viewYear, viewMonth + 1, 0);
-
-    const PLDate = prevLast.getDate();
-    const PLDay = prevLast.getDay();
-
-    const TLDate = thisLast.getDate();
-    const TLDay = thisLast.getDay();
-
-    const prevDates = [];
-    const thisDates = [...Array(TLDate + 1).keys()].slice(1);
-    const nextDates = [];
-
-    document.querySelector('.year-month').textContent = `${viewYear}년 ${viewMonth + 1}월`;
-
-    if (PLDay !== 6) {
-        for (let i = 0; i < PLDay + 1; i++) {
-          prevDates.unshift(PLDate - i);
-        }
-      }
-      
-      for (let i = 1; i < 7 - TLDay; i++) {
-        nextDates.push(i);
-      }
-
-      const dates = prevDates.concat(thisDates, nextDates);
-
-    dates.forEach((date, i) => {
-      dates[i] = `<div class="date">${date}</div>`;
-    })
-
-    document.querySelector('.dates').innerHTML = dates.join('');
-    return(
-      <div class="main">
-        <div class="days">
-            <div class="day">일</div>
-            <div class="day">월</div>
-            <div class="day">화</div>
-            <div class="day">수</div>
-            <div class="day">목</div>
-            <div class="day">금</div>
-            <div class="day">토</div>
-        </div>
-        <div class="dates"></div>
-        </div>
-    );
 }
 
 export default App;
